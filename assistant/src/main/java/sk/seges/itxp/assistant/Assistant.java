df11599f8a3cc6aa9a5072ee2c61aba4c984c992
Ladislav Gazo <gazo@seges.sk>
2013-04-07 23:11:43 +0200

    conditions, dates, expections

--- assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
+++ assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
@@ -3,12 +3,21 @@
  */
 package sk.seges.itxp.assistant;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author ladislav.gazo
  */
 public class Assistant {
-	public String[] whatShouldIDo() {
-		return new String[] { "call Darth Vader", "program R2D2", "drink Ol'Janx Spirit" }; 
+	// notice we introduced new class type - java.util.Date
+	public String[] whatShouldIDo(Date when) {
+		if(when.before(new Date())) {
+			return new String[] { "call Darth Vader", "program R2D2", "talk to Jabba" };
+		} else {
+			return new String[] { "jump into the Heart of Gold", "don't forget your towel" };
+		}
 	}
 	
 	/**
@@ -18,7 +27,16 @@ public class Assistant {
 		System.out.println("What should I do?");
 		
 		Assistant assistant = new Assistant();
-		String[] taskList = assistant.whatShouldIDo();
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+		Date when;
+		try {
+			when = formatter.parse("02.12.2014");
+		} catch (ParseException e) {
+			throw new RuntimeException("Houston ...", e);
+		}
+		
+		String[] taskList = assistant.whatShouldIDo(when);
 		
 		for(String task : taskList) {
 			System.out.println("* " + task);
