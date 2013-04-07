64084388ca1a286544c799c8fab328f1f7bad2f9
Ladislav Gazo <gazo@seges.sk>
2013-04-07 23:35:16 +0200

    multiple instances and constructors

--- assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
+++ assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
@@ -5,63 +5,62 @@ package sk.seges.itxp.assistant;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
  * @author ladislav.gazo
  */
 public class Assistant {
-	// introduced class field
 	private String[] taskList;
 	
-	// introduced constructor
 	public Assistant() {
 		taskList = new String[] { "call Darth Vader", "program R2D2",
 				"talk to Jabba", "jump into the Heart of Gold",
 				"don't forget your towel" };
 	}
 	
-	// notice we introduced new class type - java.util.Date
-	public String[] whatShouldIDo(Date when) {
-		if(when.before(new Date())) {
-			return copyRange(0, 2);
-		} else {
-			return copyRange(3, 4);
-		}
+	// non-default constructor
+	public Assistant(String... taskList) {
+		// introduced "this"
+		this.taskList = taskList;
 	}
 	
-	// introduced private method
-	private String[] copyRange(int from, int to) {
-		// verify input
-		if(from < 0 || from >= taskList.length) {
-			throw new IllegalArgumentException("From does not fit into the actual task list");
+	// used Java's Arrays class
+	public String[] whatShouldIDo(Date when) {
+		if(when.before(new Date())) {
+			return Arrays.copyOfRange(taskList, 0, 3);
+		} else {
+			return Arrays.copyOfRange(taskList, 3, 5);
 		}
-		if(to < 0 || to < from || to >= taskList.length) {
-			throw new IllegalArgumentException("To does not fit into the actual task list or is below from");
-		}
-			
-		String[] result = new String[to - from + 1];
-		
-		int j = 0;
-		for(int i = from; i <= to; i++) {
-			result[j++] = taskList[i];
-		}
-		
-		return result;
 	}
+
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("What should I do?");
+		System.out.println("What should I do intergalactic commander?");
 		
-		Assistant assistant = new Assistant();
+		// rename function...
+		Assistant galacticAssistant = new Assistant();
 		
-		// refactored to separate method + it is static because read from main
+		showMeTasks(galacticAssistant);
+		
+		System.out.println("What should I do my duke?");
+		
+		// second instance
+		Assistant fantasyAssistant = new Assistant("call Robert Baratheon", "prepare tournament",
+				"talk to Daenerys", "jump into the ship",
+				"don't forget your sword");
+		
+		showMeTasks(fantasyAssistant);
+	}
+
+	private static void showMeTasks(Assistant galacticAssistant) {
 		Date when = readWhen();
 		
-		String[] taskList = assistant.whatShouldIDo(when);
+		String[] taskList = galacticAssistant.whatShouldIDo(when);
 		
 		for(String task : taskList) {
 			System.out.println("* " + task);
@@ -72,7 +71,7 @@ public class Assistant {
 		SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
 		Date when;
 		try {
-			when = formatter.parse("02.12.2004");
+			when = formatter.parse("02.12.2014");
 		} catch (ParseException e) {
 			throw new RuntimeException("Houston ...", e);
 		}
