17f341b172ca7aa0be56a4cb5f1a9eb405fc297a
Ladislav Gazo <gazo@seges.sk>
2013-04-07 23:25:05 +0200

    what can I do in a class

--- assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
+++ assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
@@ -11,13 +11,43 @@ import java.util.Date;
  * @author ladislav.gazo
  */
 public class Assistant {
+	// introduced class field
+	private String[] taskList;
+	
+	// introduced constructor
+	public Assistant() {
+		taskList = new String[] { "call Darth Vader", "program R2D2",
+				"talk to Jabba", "jump into the Heart of Gold",
+				"don't forget your towel" };
+	}
+	
 	// notice we introduced new class type - java.util.Date
 	public String[] whatShouldIDo(Date when) {
 		if(when.before(new Date())) {
-			return new String[] { "call Darth Vader", "program R2D2", "talk to Jabba" };
+			return copyRange(0, 2);
 		} else {
-			return new String[] { "jump into the Heart of Gold", "don't forget your towel" };
+			return copyRange(3, 4);
 		}
+	}
+	
+	// introduced private method
+	private String[] copyRange(int from, int to) {
+		// verify input
+		if(from < 0 || from >= taskList.length) {
+			throw new IllegalArgumentException("From does not fit into the actual task list");
+		}
+		if(to < 0 || to < from || to >= taskList.length) {
+			throw new IllegalArgumentException("To does not fit into the actual task list or is below from");
+		}
+			
+		String[] result = new String[to - from + 1];
+		
+		int j = 0;
+		for(int i = from; i <= to; i++) {
+			result[j++] = taskList[i];
+		}
+		
+		return result;
 	}
 	
 	/**
@@ -28,19 +58,25 @@ public class Assistant {
 		
 		Assistant assistant = new Assistant();
 		
-		SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
-		Date when;
-		try {
-			when = formatter.parse("02.12.2014");
-		} catch (ParseException e) {
-			throw new RuntimeException("Houston ...", e);
-		}
+		// refactored to separate method + it is static because read from main
+		Date when = readWhen();
 		
 		String[] taskList = assistant.whatShouldIDo(when);
 		
 		for(String task : taskList) {
 			System.out.println("* " + task);
 		}
+	}
+
+	private static Date readWhen() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+		Date when;
+		try {
+			when = formatter.parse("02.12.2004");
+		} catch (ParseException e) {
+			throw new RuntimeException("Houston ...", e);
+		}
+		return when;
 	}
 
 }
