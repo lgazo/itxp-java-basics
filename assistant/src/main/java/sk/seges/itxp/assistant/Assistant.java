4b50497af6ef87c466392c4b4b564e756d840d9d
Ladislav Gazo <gazo@seges.sk>
2013-04-07 23:00:56 +0200

    first public method

--- assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
+++ assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
@@ -7,12 +7,22 @@ package sk.seges.itxp.assistant;
  * @author ladislav.gazo
  */
 public class Assistant {
-
+	public String[] whatShouldIDo() {
+		return new String[] { "call Darth Vader", "program R2D2", "drink Ol'Janx Spirit" }; 
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("How can I assist you?");
+		System.out.println("What should I do?");
+		
+		Assistant assistant = new Assistant();
+		String[] taskList = assistant.whatShouldIDo();
+		
+		for(String task : taskList) {
+			System.out.println("* " + task);
+		}
 	}
 
 }
