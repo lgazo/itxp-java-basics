356462d6b493c6f703021c514a4e54b33fca7ee3
Ladislav Gazo <gazo@seges.sk>
2013-04-07 22:46:00 +0200

    initial project

---
+++ assistant/src/main/java/sk/seges/itxp/assistant/Assistant.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package sk.seges.itxp.assistant;
+
+/**
+ * @author ladislav.gazo
+ */
+public class Assistant {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("How can I assist you?");
+	}
+
+}
